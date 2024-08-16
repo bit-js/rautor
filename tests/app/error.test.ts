0eeda6e9fd59fef5d8c668ae74b8299a7f71f6f3
aquapi <aquaplmc@gmail.com>
2024-08-16 11:58:05 +0700

    Error handling

--- tests/app/error.test.ts
+++ tests/app/error.test.ts
@@ -5,19 +5,28 @@ const randomError = staticError();
 const msgError = dynamicError<string>();
 
 const fetch = new App()
+  // Throw error randomly for testing
   .use(() => {
     if (Math.random() < 0.3) return randomError;
     if (Math.random() < 0.6) return msgError.create('File not found');
   })
+
+  // An example route
   .get('/', () => 'Hi')
+
+  // Handle static error
   .catch(randomError, (c) => {
     c.status = 500;
-    return 'An error occured'
+    return 'An error occured';
   })
+
+  // Handle dynamic error
   .catch(msgError, (msg, c) => {
     c.status = 404;
     return msg;
   })
+
+  // Compile to a fetch function
   .compile();
 
 console.log(fetch.toString());
